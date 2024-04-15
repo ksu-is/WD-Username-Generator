1f959e50e48d29569f278cf41ad5ff3360d98d83
WillDefontaine <wdefonta@students.kennesaw.edu>
2024-04-15 11:56:56 -0400

    Update code_file.py
    Added a line to combine the word and number to make the username (line 12)
    Added a line that sets the output text to show the generated username (line 15)

--- code_file.py
+++ code_file.py
@@ -8,3 +8,7 @@ def generate_random_username():
     random_words = random.sample(usernames, k=random.randint(2, 3))
 
     random_number = random.randint(10, 4000)
+    
+    username = ''.join(random_words) + str(random_number)
+
+    output_label.config(text=username)
