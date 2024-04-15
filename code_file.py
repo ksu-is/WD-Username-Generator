f1012dd50863443da5b2e512d5f361b5498cc1af
WillDefontaine <wdefonta@students.kennesaw.edu>
2024-04-15 11:55:09 -0400

    Update code_file.py
    Added function to Generate a Random username (line 6)
    Added line to pick a random word from the list (line 8)
    Added a line to pick a random number between 10 and 4000 (line 10)

--- code_file.py
+++ code_file.py
@@ -2,3 +2,9 @@ import random
 from tkinter import *
 
 usernames = ['cool', 'awesome', 'ninja', 'hacker', 'gamer', 'wizard', 'master', 'savage', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def generate_random_username():
+
+    random_words = random.sample(usernames, k=random.randint(2, 3))
+
+    random_number = random.randint(10, 4000)
