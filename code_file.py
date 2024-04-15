c36c177f568d592ef516ddd41000da0571a18d9c
WillDefontaine <156000709+WillDefontaine@users.noreply.github.com>
2024-04-14 23:18:06 -0400

    Update code_file.py added tkinter and usernames
    I kept import random and added tkinter and a list of usernames to generate. I also removed the existing code.

--- code_file.py
+++ code_file.py
@@ -1,20 +1,4 @@
 import random
+from tkinter import *
 
-print('Welcome to your Password Generator')
-
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ~`!@#$%^&*+=:;?0123456789'
-
-number = input('Amount of passwords to generate: ')
-number = int(number)
-
-length = input('Input desired password length: ')
-length = int(length)
-
-print('\nhere are your passwords: ')
-
-for pwd in range(number):
-    passwords = ''
-    for c in range(length):
-        passwords += random.choice(chars)
-    print(passwords)
-    
+usernames = ['cool', 'awesome', 'ninja', 'hacker', 'gamer', 'wizard', 'master', 'savage']
